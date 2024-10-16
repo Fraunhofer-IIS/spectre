fefd526703bfad3080b534a1d399f50d9610fde0
Julio Emmanuel Diem Wissing <julio.wissing@iis.fraunhofer.de>
2024-10-16 17:27:53 +0200

    minor changes to increase readability

--- examples/read_spectre.py
+++ examples/read_spectre.py
@@ -1,6 +1,15 @@
 from spectre.dataset import SPECTRE
 
 if __name__ == "__main__":
-    dataset = SPECTRE("/Users/wgj/SPECTRE/spectre-public")
+    dataset = SPECTRE(
+        "path/to/data",
+        int_time=1400,
+        shuffle=True,
+        cache_dir=None,
+        clean_threshold=0.15,
+        lamb_range=(450, 690),
+        cache=True,
+        augment=True,
+    )
     dataset_l = list(dataset)
     print(len(dataset_l))
